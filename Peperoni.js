b74748ba790d7484cd9e4c03b9b5b11f575731ae
Ditador <nicholaspcalamita@gmail.com>
2017-09-23 12:30:45 -0300

    Add files via upload

--- Peperoni.js
+++ Peperoni.js
@@ -6,9 +6,10 @@ var ra
 function setup() {
     createCanvas(500,500);
     background(0);
+    fundo = loadImage('Bandeira_Astro-Hungara.png')
     
-    nl = 30;
-    bl = 30;
+    nl = 10;
+    bl = 10;
     ra = 20;
     ve = 0.05;
 }
@@ -16,20 +17,30 @@ function draw() {
     background(0);
     
     for (var r = 0; r < nl; r++){
+        fill (255);
+        beginShape(TRIANGLE_STRIP);
+        texture(fundo);
         for (var i = 0; i < bl; i++){
-            beginShape(TRIANGLE_STRIP);
-            var cx = map(i, 0, bl, -100, width + 100);
-            var cy = map(r, 0, nl, -100, height + 100);
+            var cx = map(i, 0, bl, 0, width   );
+            var cy = map(r, 0, nl, 0, height  );
+            var nx = map(i, 0, bl, 0, width   );
+            var ny = map(r+1, 0, nl, 0, height);
             
             var dc = dist(cx, cy, width/2, height/2);
             var an = frameCount * ve + dc*0.02 + i*0.1;
             
             var x = cx + ra * cos(an);
             var y = cy + ra * sin(an);
-            
-            fill (255);
             vertex(x, y); 
+        
+            var dc = dist(nx, ny, width/2, height/2);
+            var an = frameCount * ve + dc*0.02 + i*0.1;
+            
+            var x = nx + ra * cos(an);
+            var y = ny + ra * sin(an);
+            vertex(x, y); 
+        
         }
+        endShape();
     }
-    endShape();
 }
